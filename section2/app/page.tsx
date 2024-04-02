5aba44b1428f4979879273ef7b1c4af0f0f575b3
Inhoob <inhoob@gmail.com>
2024-04-02 09:06:56 +0900

    section2 : 프로젝트 생성

---
+++ section2/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      <h1>Welcome to this Course!!</h1>
+      <p>Let&apos;s get started!</p>
+    </main>
+  );
+}
