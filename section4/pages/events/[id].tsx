f09d65982d157df4997ef479315bb0e0792348c3
Inhoob <inhoob@gmail.com>
2024-06-20 23:37:26 +0900

    section3 마무리

--- section4/pages/events/[id].tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-
-function Event() {
-  const router = useRouter();
-  const event = router.query;
-  return <h1>this event is {event.id}</h1>;
-}
-
-export default Event;
